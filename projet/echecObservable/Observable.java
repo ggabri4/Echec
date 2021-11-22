4e0f9f492cc9f61685081f16873ed300db44a136
GabrielGruit <ggabri432@gmail.com>
2021-11-22 22:59:43 +0100

    comments + deleting spacings

--- projet/echecObservable/Observable.java
+++ projet/echecObservable/Observable.java
@@ -5,12 +5,8 @@ import java.util.Observer;
 
 public interface Observable {
 	
-	
+	//Méthodes à redéfinir
 	public void addObserver();
-	
-	
 	public void removeObserver(Observer obs);
-	
-	
 	public void notifyObserver(ArrayList<String> moves);
 }
